76b2794e93cb314c590a03d8e17de5908905d4e9
Bibhav48 <76898850+Bibhav48@users.noreply.github.com>
2023-09-11 23:28:15 +0545

    first

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+import os
+
+app = Flask(__name__)
+@app.route('/notes')
+def notes():
+    files =[]
+    folder_path = "static/notes/physics"
+    if os.path.exists(folder_path):
+        file_names = sorted(os.listdir(folder_path))
+        files = [(i," ".join(f.split(" ")[1::]).split(".")[0]) for i,f in enumerate(file_names,start=1)]
+        links = [f"{folder_path}/{file}" for file in file_names]
+        files_phy=zip(files,links)
+    return render_template("notes.html", active="notes",files_phy=files_phy)
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
